f27b264d4d110fe3b07b162c9abf16ddb8b1a8b8
Ronnie Chong <ronnie.chong@catchoftheday.com.au>
2014-10-13 15:30:22 +1100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+ 
+        compass: {                  
+            dist: {                  
+              options: {              
+                sassDir: 'src/',
+                cssDir: 'build/',
+                environment: 'development'
+              }
+            }
+        },
+
+        watch: {
+             default: {
+                 files: ['./src/**/*.scss', './src/**/*.jade','./build/**/*.html'],
+                 tasks: ['compass', 'newer:jade', 'newer:premailer','newer:prettify']
+             }
+        },
+
+        premailer: {
+            options: {
+                mode           : 'html',
+                baseUrl        : '',
+                queryString    : '',
+                css            : [],
+                removeClasses  : false,
+                removeScripts  : true,
+                removeComments : true,
+                preserveStyles : false,
+                lineLength     : 65,
+                ioException    : false,
+                verbose        : true
+            },
+            files: {
+                expand : true,
+                src    : ['./build/**/*.html', '!./build/**/*-inline.html'],
+                dest   : './build/',
+                rename : function(dest, src) {
+                    var folder   = src.substring(0, src.lastIndexOf('/')),
+                        filename = src.substring(src.lastIndexOf('/'), src.length);                   
+
+                    filename  = filename.substring(0, filename.lastIndexOf('.'));
+
+                    return folder + filename + '-inline.html';
+                }
+            }
+        },
+
+        prettify: {
+            options: {
+                "condense"              : true,
+                "preserveBOM"           : false,
+                "indent"                : 4,
+                "indent_char"           : " ",
+                "indent_scripts"        : "normal",
+                "wrap_line_length"      : 0,
+                "brace_style"           : "collapse",
+                "preserve_newlines"     : false,
+                "unformatted"           : ["a","code","pre","span"]
+            },
+            all: {
+                expand : true,
+                ext    : '.html',
+                cwd    : 'build/',
+                src    : ['**/*.html'],
+                dest   : 'build/'
+            }
+        },
+
+        jade: {
+          compile: {
+            options: {
+              pretty:true,
+              data: {
+                debug: false
+              }
+            },
+            files: [ {
+                  cwd: "src/",
+                  src: ["**/*.jade", "!core/**/*.jade"],
+                  dest: "build/",
+                  expand:true,
+                  ext: ".html"
+                }]
+          }
+        }
+ 
+    });
+ 
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-premailer');
+    grunt.loadNpmTasks('grunt-prettify');
+
+    grunt.registerTask('default',['watch']);
+ 
+};
---
+++ README.md
@@ -0,0 +1,38 @@
+#EDM automation#
+This is a simple EDM automation to convert Jade/Compass content into email friendly HTML template.
+
+There will be two HTML copies, one standard html with external css file and one with inline style css
+
+##Installation##
+
+###Prerequisites###
+
+Gem Premailer
+```
+gem install premailer
+```
+Node
+http://nodejs.org/download/
+
+Grunt
+```
+npm install grunt-cli
+```
+
+_Note: You may need to ```sudo``` to install_
+
+### Running the build ###
+
+```grunt```
+
+##Instructions##
+* Grunt watches any changes in Jade/SCSS and the HTML output
+* Source files are in```src```
+* Compiled files are exported to ```build```
+
+##Notes##
+* Sample EDM design is semi-responsive
+* Only newer/modified files are compiled
+* Inline CSS HTML has a postfix ```*-inline.html```
+
+
